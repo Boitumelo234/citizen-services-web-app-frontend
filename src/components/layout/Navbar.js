099d8fbe52693c3d99a9905c42be8a9c250d0651
Modipadi-Rapudi <rapudimodipadi9@gmail.com>
2026-02-24 14:20:22 +0200

    Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,24 +1,38 @@
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import "./Navbar.css";
 
 function Navbar() {
     return (
         <nav className="navbar">
-            <h2>Rustenburg Municipality</h2>
-
-            <div className="nav-links">
-                <Link to="/">Home</Link>
-                <Link to="/services">Services</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to="/faq">FAQ</Link>
-
-                <Link to="/login" className="nav-btn login-btn">
-                    Login
+            <div className="navbar-container">
+                {/* Logo Section */}
+                <Link to="/" className="navbar-logo-container">
+                    <img
+                        src={logo}
+                        alt="Rustenburg Municipality Logo"
+                        className="navbar-logo"
+                    />
+                    <span className="navbar-title">Rustenburg Municipality</span>
                 </Link>
 
-                <Link to="/register" className="nav-btn register-btn">
-                    Register
-                </Link>
+                {/* Navigation Links */}
+                <div className="nav-links">
+                    <Link to="/">Home</Link>
+                    <Link to="/services">Services</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/contact">Contact</Link>
+                    <Link to="/faq">FAQ</Link>
+
+                    <div className="nav-auth-buttons">
+                        <Link to="/login" className="nav-btn login-btn">
+                            Login
+                        </Link>
+                        <Link to="/register" className="nav-btn register-btn">
+                            Register
+                        </Link>
+                    </div>
+                </div>
             </div>
         </nav>
     );
